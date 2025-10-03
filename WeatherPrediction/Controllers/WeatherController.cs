56d0b4eaf28e32956fa35159deb9dc9bcc13df9a
kareem <kareemsameh1670@gmail.com>
2025-10-03 15:08:18 +0300

    Adding controllers GoogleAI &  Weather

---
+++ WeatherPrediction/Controllers/WeatherController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WeatherPrediction.BLL.DTOs;
+using WeatherPrediction.BLL.Interfaces;
+
+namespace WeatherPrediction.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly IWeatherProbabilityService _weatherService;
+
+        public WeatherController(IWeatherProbabilityService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<WeatherPredictionResult>> GetDailyMean(
+         [FromQuery] float lat, [FromQuery] float lon, [FromQuery] DateTime date, [FromQuery] bool HigherAccuracy = true)
+        {
+            var result = await _weatherService.GetDailyProbabilities(lat, lon, date, HigherAccuracy);
+            if (result == null)
+                return NotFound("Could not fetch weather data from NASA API.");
+
+            return Ok(result);
+        }
+
+    }
+}
